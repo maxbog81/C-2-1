d8d4eb50a9e4e06247d6277c3711e0341c5287a6
maxbog81 <maxbog@inbox.ru>
2019-04-20 00:44:30 +0300

    Урок 1 по с-2

---
+++ MyGame/MyGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+
+        }
+    }
+}
